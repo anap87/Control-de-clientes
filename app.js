7fa917be4414e289b2b64e616bab26647d6d938b
anap87 <apor2209@gmail.com>
2026-01-05 10:41:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // Login simple (sin backend por ahora)
+  window.location.href = "dashboard.html";
+});
 
